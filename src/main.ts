3c4877bc16fb1377efad053dba984f98458d2e36
Louis Parent <contact@louis-parent.fr>
2024-02-05 00:45:22 +0100

    Changing layout

--- src/main.ts
+++ src/main.ts
@@ -5,33 +5,28 @@ import Controller from "./utils/controller";
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>Tamiyos' Journal</h1>
   
-  <div class="row spaced full-width bottom-margin-midrange">
-    <div class="column at-start right-margin-near">
-      <h2 class="no-margin">Card set :</h2>
+  <div class="row gapped full-width">
+    <div class="column">
       <set-selector id="set"></set-selector>
-    </div>
-
-    <div class="column at-start">
-      <h2 class="no-margin">Card number :</h2>
       <input id="card" type="number" min="1" step="1" required placeholder="Collector number" />
-    </div>
-
-    <div class="column at-start">
-      <h2 class="no-margin">Card language :</h2>
       <select id="language" required>
         <option value="en" selected>🇺🇸 English</option>
         <option value="fr">🇫🇷 French</option>
         <option value="ph">Phyrexian</option>
       </select>
     </div>
+    
+    <div class="column">
+      <img id="preview" class="quarter-screen-width" />
+
+      <div class="row spaced full-width bottom-margin-midrange">
+        <button id="add">➕</button>
+        <button id="remove">➖</button>
+      </div>
+    </div>
   </div>
 
-  <div class="row spaced full-width bottom-margin-midrange">
-    <button id="add">➕</button>
-    <button id="remove">➖</button>
-  </div>
 
-  <img id="preview" class="third-width-max" />
 `;
 
 setTimeout(() => {
