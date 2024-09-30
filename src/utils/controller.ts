12e8dc20300d4a0a45ad96c75ad5e3dc990972a2
Louis Parent <contact@louis-parent.fr>
2024-09-30 21:48:47 +0200

    Fix https

--- src/utils/controller.ts
+++ src/utils/controller.ts
@@ -194,7 +194,7 @@ export default class Controller extends Listenable(Object) {
 	}
 
 	private async importListener() {
-		const bulkDataUrl = 'http://lirmalys.ovh/TamiyosHelper/all_cards.json';
+		const bulkDataUrl = 'https://lirmalys.ovh/TamiyosHelper/all_cards.json';
 
 		const response = await fetch(bulkDataUrl);
 		const data = await response.json();
